56d05c7e4c3d1c691f46a5cb48440197e569a488
Drew Wilson <drewwilson@Drews-Air.modem>
2021-02-21 14:58:40 +1100

    added code
    done

---
+++ yahtzee_dice.py
@@ -0,0 +1,9 @@
+import random as rand
+
+dice1 = rand.randint(1,6)
+dice2 = rand.randint(1,6)
+dice3 = rand.randint(1,6)
+dice4 = rand.randint(1,6)
+dice5 = rand.randint(1,6)
+
+print(f"Dice 1: {dice1} \nDice 2: {dice2} \nDice 3: {dice3} \nDice 4: {dice4} \nDice 5: {dice5}")
